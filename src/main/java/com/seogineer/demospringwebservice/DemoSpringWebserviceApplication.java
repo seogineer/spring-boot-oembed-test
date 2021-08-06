21e465f07528533a221ab1d245430c62db3ad8a4
seogineer <dgseo8981@gmail.com>
2021-08-06 14:48:01 +0900

    프로젝트 생성

---
+++ src/main/java/com/seogineer/demospringwebservice/DemoSpringWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.seogineer.demospringwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringWebserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringWebserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/seogineer/demospringwebservice/DemoSpringWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seogineer.demospringwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringWebserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
